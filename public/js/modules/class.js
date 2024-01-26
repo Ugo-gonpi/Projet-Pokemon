40ce3066998e7b78aedade455f0f4da0d54e731e
Ugo-gonpi <ugogonzalez50@gmail.com>
2024-01-26 12:05:23 +0100

    did most of the attcaks for the pokemons

--- public/js/modules/class.js
+++ public/js/modules/class.js
@@ -2,17 +2,41 @@ export let Aquali = {
     nom : "Aquali",
     type : "Eau",
     niveau : 100,
-    pv : 380,
-    pAtk : 55,
+    pv : 390,
+    pAtk : 60,
 
     icebeam() {
-        if (this.pv > 0 ) {
-            this.icebeam = Luxray.pv - this.pAtk
+        if (this.pv > 0 && Luxray.pv > 0) {
+            Luxray.pv = Luxray.pv - this.pAtk
+            this.pAtk = 55
         } else {
-            
-        }
-    }
 
+        }
+    },
+
+    dig() {
+        if (this.pv > 0 && Luxray.pv > 0) {
+            Luxray.pv = Luxray.pv - this.pAtk
+            this.pAtk = 55
+        } else {
+
+        }
+    },
+
+    hydroPump() {
+        if (this.pv > 0 && Luxray.pv > 0) {
+            Luxray.pv = Luxray.pv - this.pAtk + 25
+            this.pAtk = 55
+        } else {
+
+        }
+    },
+
+    calmMind() {
+        if (this.pv > 0 && Luxray.pv > 0) {
+            this.pAtk = this.pAtk * 2
+        }
+    },
 }
 
 export let Luxray = {
@@ -20,7 +44,39 @@ export let Luxray = {
     type : "Electric",
     niveau : 100,
     pv : 360,
-    pAtk : 70,
+    pAtk : 80,
 
+    wildCharge() {
+        if (this.pv > 0 && Aquali.pv > 0) {
+            Aquali.pv = Aquali.pv - this.pAtk + 25
+            this.pv = this.pv - this.pAtk / 4
+        } else {
+
+        }
+    },
+
+    crunch() {
+        if (this.pv > 0 && Aquali.pv > 0) {
+            Aquali.pv = Aquali.pv - this.pAtk
+        } else {
+
+        }
+    },
+
+    playRough() {
+        if (this.pv > 0 && Aquali.pv > 0) {
+            Aquali.pv = Aquali.pv - this.pAtk
+        } else {
+
+        }
+    },
+
+    fireFang() {
+        if (this.pv > 0 && Aquali.pv > 0) {
+            Aquali.pv = Aquali.pv - this.pAtk / 2
+        } else {
+
+        }
+    },
     
 }
